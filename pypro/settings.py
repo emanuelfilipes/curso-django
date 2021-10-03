03be7c724e22be726371f530ce4957795a86cbf1
Emanuel <emanuelfilipess@gmail.com>
2021-10-03 19:01:49 -0300

    Configurando admin
    close #31

--- pypro/settings.py
+++ pypro/settings.py
@@ -135,7 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 COLLECTFAST_ENABLED = False
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=False)
 
 # STORAGE CONFIGURATION IN S3 AWS
 # ------------------------------------------------------------------------
