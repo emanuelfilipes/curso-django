76163f822e828ac1086a7857b755960b79935527
Emanuel <emanuelfilipess@gmail.com>
2021-10-30 19:01:01 -0300

    Aplicação filtros no titulo do vídeo close #51

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def video(request, slug):
     video = {
         'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 640497475},
-        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 640754710},
+        'instalacao-windows': {'titulo': '', 'vimeo_id': 640754710},
     }
     video = video[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
