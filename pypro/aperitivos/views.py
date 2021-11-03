5b6c400654801cbd5ba2edcd927c4fdc81636703
Emanuel <emanuelfilipess@gmail.com>
2021-11-02 21:12:31 -0300

    Aplicação de lista de elementos com for close #53

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 640497475},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 640754710},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    video = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 640497475},
-        'instalacao-windows': {'titulo': '', 'vimeo_id': 640754710},
-    }
-    video = video[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
