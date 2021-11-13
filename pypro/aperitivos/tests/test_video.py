dd5b96e09d9afe409225013e04995e551c495960
Emanuel <emanuelfilipess@gmail.com>
2021-11-13 15:21:31 -0300

    De Executar Testes Com e Sem Migrações até Introdução ao Admin do Django close #67

---
+++ pypro/aperitivos/tests/test_video.py
@@ -0,0 +1,38 @@
+import pytest
+from django.urls import reverse
+
+from pypro.aperitivos.models import Video
+from pypro.django_assertions import assert_contains
+from model_mommy import mommy
+
+
+
+@pytest.fixture
+def video(db):
+    return mommy.make(Video)
+
+
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
+
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
+
+
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo)
+
+
+def test_conteudo_video(resp, video):
+    assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}?h=a7afa8f2f9&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"')
