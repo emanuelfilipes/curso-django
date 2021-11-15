b6356e4b4936d123bac1f53d998e43fa06d1639e
Emanuel <emanuelfilipess@gmail.com>
2021-11-15 12:50:02 -0300

    Construção da Página de Detalhes do Módulo close #71

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
-def detalhe(request, slug):
-    pass
+from pypro.modulos import facade
 
+
+def detalhe(request, slug):
+    modulo = facade.encontrar_modulo(slug)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
