a4acccc81cfc3cc5a01f72747fdf40e5bbf83ee9
Emanuel <emanuelfilipess@gmail.com>
2021-11-18 02:46:12 -0300

    Adicionando Acesso ao lado N do relacionamento até Objeto Prefetch close #83

--- pypro/modulos/tests/test_modulo_indice.py
+++ pypro/modulos/tests/test_modulo_indice.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 from django.urls import reverse
 from pypro.django_assertions import assert_contains
@@ -29,23 +31,31 @@ def test_indice_disponivel(resp):
     assert resp.status_code == 200
 
 
-# def test_titulo(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.titulo)
-#
-#
-# def test_descricao(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.descricao)
-#
-#
-# def test_publico(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.publico)
-#
-#
-# def test_aulas_titulos(resp, aulas):
-#     for aula in aulas:
-#         assert_contains(resp, aula.titulo)
-#
-#
-# def test_aulas_links(resp, aulas):
+def test_titulo(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.publico)
+
+
+def test_aulas_titulos(resp, aulas: List[Aula]):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
+
+
+def test_aulas_urls(resp, aulas: List[Aula]):
+    for aula in aulas:
+        assert_contains(resp, aula.get_absolute_url())
+
+
+# def test_aulas_links(resp, aulas): # não tinha na aula de Acesso ao lado N do Relacionamento(nome diferente(test_aula_url))
 #     for aula in aulas:
 #         assert_contains(resp, aula.get_absolute_url())
